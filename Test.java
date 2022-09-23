8b3bc69fbf922e363fd1c6bab9dc96e2cc4a6e5d
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-09-23 11:54:28 +0800

    My Name ??

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 public class Test{
 	public static void main(String[] args){
-        System.out.println(args [1]);
-        System.out.println(args [2]);
-        System.out.println("Second Version");
-}
+        System.out.println(args[0]);
+        printStr("willy");
+   }
+    public static void printStr(String str){
+        System.out.println("My Name = " + str);
+   }
 }
