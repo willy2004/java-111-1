ead8b850d495dce0e3acb048c0ee72892f08374d
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-09-23 11:39:59 +0800

    Second Version

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
 	public static void main(String[] args){
-		System.out.println(args [0]);
-		System.out.println(args [1]);
-		System.out.println(args [2]);
-	}
+        System.out.println(args [1]);
+        System.out.println(args [2]);
+        System.out.println("Second Version");
+}
 }
