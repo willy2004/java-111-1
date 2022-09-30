f34f81e5b6a1b77d52549d31bce240da916671b9
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-09-30 11:04:51 +0800

    Judgment  (if)

--- Test.java
+++ Test.java
@@ -3,22 +3,14 @@ import java.util.Scanner;
 public class Test{
 	public static void main(String[] args){
           int num1;
-          int num2;
-          int sum;
-       
+          
        Scanner input = new Scanner(System.in);
        System.out.print("Enter first integer:");
        num1 = input.nextInt();
-       
-       System.out.print("Enter first integer:");
-       num2  = input.nextInt();
-       
-       sum = num1 + num2;
-       System.out.print("The sum is " + sum  );
-       
+
+       if (num1 >= 60){
+       System.out.print("Good");
+       }
        
    }
-   /*  public static void printStr(int str){
-        System.out.println(str);
-   } */
 }
