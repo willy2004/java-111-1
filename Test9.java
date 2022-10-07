213daffb015dbf6ccae399529ee61744a9e437c4
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-10-07 11:40:55 +0800

    10/7(迴圈1-9)

---
+++ Test9.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test9{
+	public static void main(String[] args){
+            int grade = 0;
+            String space = "";
+             
+            while(grade < 10){ 
+                System.out.printf("%s", space); 
+                System.out.printf("%d%n", grade);
+                grade = grade + 1;
+                space = apace + " ";   
+}       
+}   
+}  　
