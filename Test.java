dc843beb5bcad0dd9ad4b4bf34ba59917e3e2d32
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-10-07 08:50:05 +0800

    10/7 java

--- Test.java
+++ Test.java
@@ -4,16 +4,26 @@ public class Test{
 	public static void main(String[] args){
           int num1;
           
-       Scanner input = new Scanner(System.in);
+       Scanner input = new Scanner(System.in); 
        System.out.print("Enter first integer:");
        num1 = input.nextInt();
 
-       if (num1 >= 60 & num1 <= 70){
-          System.out.print(" <=70 "); 
-          }else{
-          System.out.print(" <60 ");    
+       if (num1 >= 90 & num1 <= 100){
+          System.out.print(" A "); 
           }
+	   if   (num1 >= 80 & num1 <= 89){  
+		 System.out.print(" B ");}    
+          
+	    if  (num1 >= 70 & num1 <= 79){    
+		 System.out.print(" C "); }   
+          
+	    if  (num1 >= 60 & num1 <= 69){    
+		 System.out.print(" D ");}    
+          
+		if  (num1 <= 60){    
+		 System.out.print(" E "); }	  
+		  
        
           
-       }  
+		  }  
    }
