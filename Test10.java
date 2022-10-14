63e3c341fc8bbbeffea79e86133e47b6f651ab07
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-10-14 11:50:02 +0800

    java (*1-5)

---
+++ Test10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test10{
+	public static void main(String[] args){
+		int row = 0;
+
+		while(row < 5){
+			 int star =0;
+			 while (star <= row){
+			   System.out.print("*");
+			   star = star + 1;
+			}
+			System.out.println("");
+			row = row + 1; 
+		}		
+		   			
+    }   
+}  
