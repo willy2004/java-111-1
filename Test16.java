4b01c6b33571adec9cf3dd258692839c528c0d36
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-10-21 11:28:57 +0800

    java -(for 1+到100)10/21

---
+++ Test16.java
@@ -0,0 +1,9 @@
+public class Test16{
+    public static void main(String[] args){
+        int b=0;
+        for(int a=0;a<101;a++){
+            b+=a;
+        }
+        System.out.printf("%d",b);
+    }
+}
