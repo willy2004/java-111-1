543022c9f1d7363108df265458460d2f9c1ba540
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-10-21 10:04:01 +0800

    java - (for 9*9乘法)10/21

---
+++ Test14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+    public class Test14{
+        public static void main(String[] args){
+         
+
+         for ( int i=1; i<=9 ; i++ ){
+           for(int j=1; j<=9 ; j++){  
+            System.out.printf("%d*%d=%d\t ", j,i ,j*i);
+           }
+            System.out.printf("\n");
+         }
+         }
+    }
