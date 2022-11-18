e65c140501c999ed3df51d5ae00a8b9f7ab6b597
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-11-18 08:53:03 +0800

    java 11/18 (java 副程式)

---
+++ Test19.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+    public class Test19{
+        public static void main(String[] args){
+			int a;
+			int k=7;
+			
+			system.out printf("%d%n",k);
+			a=add(k,4);
+	        system.out printf("%d%n",k);
+
+         }
+		  public static  int add(int x,int y){
+			 //int t = add(x,y);
+            int t = x + y;
+            return  t ;			
+		  }
+    }
