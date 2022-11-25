f1dddc8c23269d7bb3a0c2fb706ef2592f35fc59
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-11-25 10:10:46 +0800

    java  (空心菱形 學生解)11/25

---
+++ Test21.java
@@ -0,0 +1,16 @@
+public class Test21{
+    public static void main (String[] avgs){//輸入
+   
+        for(int i= -4;i<=4;i++){
+            for(int k= -5; k<=5;k++){
+                if(Math.abs(k)+ Math.abs(i) > 4) 
+                    System.out.print("*");
+                else
+                    System.out.print(" ");                   
+                     }
+                    System.out.println(""); 
+					
+
+		}
+	}
+}
