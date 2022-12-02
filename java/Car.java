134aefe58eef88c801151e7180839a388d038403
willy2004 <114204308+willy2004@users.noreply.github.com>
2022-12-02 11:24:10 +0800

    java (Car,User 陣列題目)

---
+++ java/Car.java
@@ -0,0 +1,13 @@
+public class Car{ 
+    public int whee11;
+	public int whee12;
+	public int whee13;
+	public int whee14;
+	
+	public int launch(int key){
+		if(key == 7)
+			return 1;
+		else
+		    return 0;
+	}
+}
---
+++ java/User.java
@@ -0,0 +1,17 @@
+public class User{
+	public static void main (String[] args){
+		//Car myCar = new Car();
+		int[] iArr = {7,3,4,9,6};
+		
+		for (int x=0; x<iArr.length; x++){
+			if(iArr[x]=x<iArr[x]=x+1)	
+ 	            iArr[x]=x;		   
+		}
+		
+		for (int x=0; x<iArr.length; x++){
+			System.out.println(">>" + iArr[x]);
+		}
+	}
+}
+			
+			
