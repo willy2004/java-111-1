44762906626a0c553801cd5d88c93b113c31d15d
willy2004 <114204308+willy2004@users.noreply.github.com>
2023-03-03 11:35:42 +0800

    java-2023/03/03(建構子)

---
+++ Human.java
@@ -0,0 +1,17 @@
+class Human{ 
+	String sex ;
+	int height ;
+	int weight ;
+	public Human(String sex ,int height ,int weight){
+		this.sex = sex;
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	public void printHight(){
+		System.out.println(height);
+	}
+	public void printWeight(){
+		System.out.println(weight);
+	}
+	}
---
+++ User10.java
@@ -0,0 +1,11 @@
+public class User10{ 
+	public static void main (String[] args){
+		Human human1 ;
+		human1 = new Human("man",180,80);
+		Human human2;
+		human2 = new Human("man",100,30);
+		human1.printHight();
+		human2.printHight();
+		
+	}
+}
