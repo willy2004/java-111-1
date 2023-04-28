bec8fb16ebc1a9adca178e113b3bbd1f2168466e
willy2004 <114204308+willy2004@users.noreply.github.com>
2023-04-28 11:37:42 +0800

    2023/0428

---
+++ Test26.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Test26{
+	public static void main(String[] args){
+		Baby tom;
+		System.out.println(tom.height);
+		 
+		}
+	}
+		
+		class Baby{
+		int height;
+		int weight;
+		String sex;
+		
+			public Baby(int height){
+				this(height,3500);
+				
+		}		
+		    public Baby(int h ,int w){
+				height = h;
+				weight = w;
+			}
+			public void sleep(){
+				System.out.println("sleep......")
+			}
+		   			
+    }  
